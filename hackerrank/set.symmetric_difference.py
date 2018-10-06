f41cad64f86ca9761ab1056a7020f28cdd9f5403
mdhvkothari <mdhvkothari007@gmail.com>
2018-10-06 19:19:39 +0530

    commit

--- hackerrank/set.symmetric_difference.py
+++ hackerrank/set.symmetric_difference.py
@@ -3,17 +3,3 @@ english_student= list(map(str,raw_input().split()))
 french_number=int(input())
 french_student = list(map(str,raw_input().split()))
 print(len(set(english_student).symmetric_difference(set(french_student))))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
