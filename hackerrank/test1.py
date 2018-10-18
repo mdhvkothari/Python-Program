f415056af25c9051bb7e7146110f19eda7d4cf19
mdhvkothari <mdhvkothari007@gmail.com>
2018-10-18 22:55:39 +0530

    commit

--- hackerrank/test1.py
+++ hackerrank/test1.py
@@ -1,7 +1,32 @@
-for _ in range(int(input())):
-    line = raw_input()
-    
-    while ' && ' in line or ' || ' in line:
-        line = line.replace(" && ", " and ").replace(" || ", " or ")
-    
-    print(line)
+t=int(input())
+for i in range(0,t):
+	s=raw_input()
+	length=[]
+	distinct=[]
+	for i in s:
+		if i not in distinct:
+			distinct.append(i)
+	if len(distinct)<3:
+		print "Dynamic"
+	else:
+		for i in range(0,len(distinct)):
+			count=0
+			for j in range(0,len(s)):
+				if s[j]==distinct[i]:
+					count+=1
+			length.append(count)
+		length.sort()
+		i=0
+		flag=0
+		while (i!=len(length)): 
+			if	length[i]==length[i-1]+length[i-2]:
+				flag=1
+			else:
+				flag=0
+			i+=1
+		if flag==1:
+			print "Dynamic"
+		else:
+			print "Not"
+
+
