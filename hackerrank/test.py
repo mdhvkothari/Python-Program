14c6ac28da4c42c1b04a491e471b56f37659a319
mdhvkothari <mdhvkothari007@gmail.com>
2018-10-25 23:49:49 +0530

    commit

--- hackerrank/test.py
+++ hackerrank/test.py
@@ -1,22 +1,14 @@
-a=[1,2,3,4,5]
-b=[6,1,2]
-c=[]
-d=[]
-for i in range(0,len(b)):
-	a.append(b[i])
+T=int(input())
+for i in range(0,T):
+	k,n=raw_input().split()
+	value=list(map(int,raw_input().strip().split(' ')))
+	impress=[]
+	i=0
+	a=int(n)
+	while (a!=len(value)):
+		x=value[i:a]
+		impress.append(sum(x))
+		i+=1
+		a+=1
 
-print(a)
-
-for j in range(0,len(a)):
-	for k in range(j,len(a)-1):
-
-		if a[k+1]==a[j]:
-			c.append(j)
-			c.append(k+1)		
-
-print(c)
-z=a[:]
-for i in c:
-	a.remove(z[i])
-
-print(a)
+print(max(impress))
--- hackerrank/test1.py
+++ hackerrank/test1.py
@@ -1,16 +1,12 @@
-n=int(input())
-value=[]
-for i in range(0,n):
-	bio = list(map(str,raw_input().strip().split(' ')))
-	value.append(bio)
-for i in range(0,len(value)):
-	for j in range(0,len(value)-1):
-		if value[j][2]>value[j+1][2]:
-			temp = value[j]
-			value[j]=value[j+1]
-			value[j+1]=temp
-for i in range(0,len(value)):
-	if value[i][3]=="M":
-		print "Mr. "+value[i][0]+" "+value[i][1]
-	else:
-		print "Ms. "+value[i][0]+" "+value[i][1]
+t = int(input())
+for i in range(0,t):
+	n=int(input())
+	v=list(map(int,input().split()))
+	a=max(v)
+	for i in range(0,n):
+		for j in range(i+1,n):
+
+			if (abs(v[i]-v[j])<a):
+				a=abs(v[i]-v[j])
+	print (a)
+
