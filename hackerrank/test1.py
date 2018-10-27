440a3127827b2ddf457b4f2a4db887396fd32302
mdhvkothari <mdhvkothari007@gmail.com>
2018-10-27 14:41:05 +0530

    commit

--- hackerrank/test1.py
+++ hackerrank/test1.py
@@ -1,12 +1,12 @@
-t = int(input())
-for i in range(0,t):
-	n=int(input())
-	v=list(map(int,input().split()))
-	a=max(v)
-	for i in range(0,n):
-		for j in range(i+1,n):
-
-			if (abs(v[i]-v[j])<a):
-				a=abs(v[i]-v[j])
-	print (a)
-
+n=int(input())
+count=0
+while (n!=0):
+	b=n%10
+	count+=1
+	n=n//10
+	if (count>3):
+		break
+if (count>3):
+	print ("More than 3 digits")
+else:
+	print (count)
