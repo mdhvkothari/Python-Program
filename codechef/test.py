cfae1909a9bc234772876fe4c0d42e6486d3031c
mdhvkothari <mdhvkothari007@gmail.com>
2019-09-13 08:44:22 +0530

    commit

--- codechef/test.py
+++ codechef/test.py
@@ -1,32 +1,25 @@
 T = int(input())
-N= int(input())
-arr=[]
-n1 = 0
-n2 = 1
-count = 0
+for i in range(T):
+    N= int(input())
+    arr=[]
+    n1 = 0
+    n2 = 1
+    count = 0
 
-if N == 1:
-   print(n1)
+    while count < N:
+        arr.append(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
 
-else:
-   while count < N:
-       arr.append(n1)
-       nth = n1 + n2
-       n1 = n2
-       n2 = nth
-       count += 1
 
-for i in range(0,N):
-    arr[i] = arr[i]%10
-
-updateArr =[]
-if(N!=0):
-    while(len(updateArr)!=1):
-        updateArr =[]
-
-        for i in range(0,len(arr)):
-            if(i%2!=0):
-                updateArr.append(arr[i]);
-        arr = updateArr
-
-    print(arr[0])
+    updateArr =[]
+    if(N!=0):
+        while(len(updateArr)!=1):
+            updateArr =[]
+            for i in range(0,len(arr)):
+                if(i%2!=0):
+                    updateArr.append(arr[i]%10);
+            arr = updateArr
+        print(arr[0])
