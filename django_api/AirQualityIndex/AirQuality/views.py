dcd0f9736f6243827ab9ff9ddd8b407d8faa51c2
mdhvkothari <mdhvkothari007@gmail.com>
2019-09-28 23:03:51 +0530

    commit

--- django_api/AirQualityIndex/AirQuality/views.py
+++ django_api/AirQualityIndex/AirQuality/views.py
@@ -18,10 +18,13 @@ def about(request):
     return render(request,'about.html')
 
 def article(request,url):
-    api_request_article = requests.get("https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=24bcccedf20d412aa67dc9693f58d729")
+    json_url = "https://newsapi.org/v2/top-headlines?sources="
+    key = "&apiKey=24bcccedf20d412aa67dc9693f58d729"
+    final = json_url+url+key
+    api_request_article = requests.get(final)
     try:
         api_article = json.loads(api_request_article.content)
     except Exception as e:
         api_article ="Error"
     data_article = api_article['articles']
-    return render(request,'article.html')
+    return render(request,'article.html',{'data_article':data_article})
