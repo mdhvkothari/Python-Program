bda7f924ce1a683c360b98919de0c736d0f1b1f3
mdhvkothari <mdhvkothari007@gmail.com>
2019-11-17 21:59:15 +0530

    commit

--- codechef/test.py
+++ codechef/test.py
@@ -1,4 +1,15 @@
-for i in range(7):
-    for j in range(i):
-        if(j==i):
-            print(j)
+N = int(input())
+customer =[]
+profit=[]
+
+for i in range(N):
+    customer_element= int(input())
+    customer.append(customer_element)
+
+customer.sort()
+
+for j in range(N):
+    profit_element = customer[j]*(N-j)
+    profit.append(profit_element)
+
+print(max(profit))
