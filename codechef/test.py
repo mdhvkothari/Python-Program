566eaf00e894393314a8b5f5ed02872b238f84c7
mdhvkothari <mdhvkothari007@gmail.com>
2020-02-01 22:34:09 +0530

    commit

--- codechef/test.py
+++ codechef/test.py
@@ -3,12 +3,22 @@ for _ in range(T):
     N = int(input())
     question=[]
     score=[]
+    result=[]
+    remaining=[]
     for _ in range(N):
-
-
-    for i in range(0,N):
-        for j in range(i,N):
-            if (i!=j):
-                if(A[i]+A[j] == A[i]*A[j]):
-                    count = count+1
-    print(count)
+        q,p = map(int,input().strip().split(" "))
+        if(q<=8):
+            question.append(q)
+            score.append(p)
+            remaining.append(p)
+    for i in range(0,len(question)):
+        for j in range(i,len(question)):
+            if(question[i]==question[j]):
+                if(score[i]>score[j]):
+                    result.append(score[i])
+                    remaining.append(score[i])
+                elif(score[i]<score[j]):
+                    result.append(score[j])
+                    remaining.append(score[j])
+    
+    print(remaining)
