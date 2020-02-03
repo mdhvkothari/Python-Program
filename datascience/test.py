6119c4a393458585e38778b1a6d1c5f16cd04b48
mdhvkothari <mdhvkothari007@gmail.com>
2020-02-03 22:55:16 +0530

    commit

---
+++ datascience/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+x1 = np.random.randint(10,size=6)
+print(x1)
+x2 = np.random.randint(10,size=(3,4))
+print(x2)
+x3 = np.random.randint(10,size=(3,4,5))
+print(x3)
+grid = np.arange(1,7).reshape((3,2))
+print(grid)
