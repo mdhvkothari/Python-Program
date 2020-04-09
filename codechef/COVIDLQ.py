09cdef169857eee65f49c397217237fea89ff793
mdhvkothari <mdhvkothari007@gmail.com>
2020-04-09 20:17:44 +0530

    commit

--- codechef/COVIDLQ.py
+++ codechef/COVIDLQ.py
@@ -1,25 +1,29 @@
 T = int(input())
-while (T>0):
-    N = int(input())
-    A = list(map(int,input().split()))
-    position = []
-    for i in range(0,len(A)):
-        if A[i]==1:
-            position.append(i+1)            
-    distance = False
-    for j in range(0,len(position)-1):
-        if (abs(position[j]-position[j+1])>3):
-            distance = True
-        else:
-            distance = False
-    if (distance == True):
-        print("Yes")
-    else:
-        print("No")
+if (T>=1 or T<=100):
+    print(T)
+    while (T>0):
+        N = int(input())
+        if (T>=1 or T<=100):
+            A = list(map(int,input().split()))
+            if A is 1 or 0:
+                position = []
+                for i in range(0,len(A)):
+                    if A[i]==1:
+                        position.append(i+1)            
+                distance = False
+                for j in range(0,len(position)-1):
+                    if (abs(position[j]-position[j+1])>3):
+                        distance = True
+                    else:
+                        distance = False
+                if (distance == True):
+                    print("Yes")
+                else:
+                    print("No")
 
-    T = T-1
+                T = T-1
 
-
-
+else:
+    print("no")
 
 
--- codechef/test.py
+++ codechef/test.py
@@ -1,25 +1,6 @@
-T = int(input())
-while (T>0):
-    N = int(input())
-    A = list(map(int,input().split()))
-    position = []
-    for i in range(0,len(A)):
-        if A[i]==1:
-            position.append(i+1)            
-    distance = False
-    for j in range(0,len(position)-1):
-        if (abs(position[j]-position[j+1])>3):
-            distance = True
-        else:
-            distance = False
-    if (distance == True):
-        print("Yes")
-    else:
-        print("No")
-
-    T = T-1
-
-
-
-
-
+n = int(input())
+def f(int n):
+    while (n):
+        reminder = n%10
+        n=n/10
+        
