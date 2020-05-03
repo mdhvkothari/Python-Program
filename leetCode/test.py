a81ba860c80e84eddf0fe1a040d710f51970feb9
mdhvkothari <mdhvkothari007@gmail.com>
2020-05-03 14:03:28 +0530

    commit

--- leetCode/test.py
+++ leetCode/test.py
@@ -1,8 +1,9 @@
-a=[1,2,3,0,0,0]
-b=[2,5,6]
-n=3
-j=0
-for i in range(n,len(a)):
-    a[i] = b[j]
-    j+=1
+a= [3,2,2,3,1]
+b=1
+i=0
+while(i<len(a)):
+    if a[i] == b:
+        a.remove(a[i])
+        print(a)
+    i+=1
 print(a)
