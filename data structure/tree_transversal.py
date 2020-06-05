a8c91fa0b2c1c5e772b8ca6f1413c08b93ef927b
mdhvkothari <mdhvkothari007@gmail.com>
2020-06-05 14:02:57 +0530

    commit

--- data structure/tree_transversal.py
+++ data structure/tree_transversal.py
@@ -16,6 +16,37 @@ class Queue:
     def size(self):
         return len(self.item)
 
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def __len__(self):
+        return self.size()
+     
+    def size(self):
+        return len(self.items)
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):  
+        if not self.is_empty():
+            return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def __str__(self):
+        s = ""
+        for i in range(len(self.items)):
+            s += str(self.items[i].value) + "-"
+        return s
+
+
 
 class Node(object):
     def __init__(self, value):
@@ -27,18 +58,6 @@ class Node(object):
 class BinaryTree(object):
     def __init__(self, root):
         self.root = Node(root)
-
-    def print_tree(self, traversal_type):
-        if traversal_type == "preorder":
-            return self.preorder_print(tree.root, "")
-        elif traversal_type == "inorder":
-            return self.inorder_print(tree.root, "")
-        elif traversal_type == "postorder":
-            return self.postorder_print(tree.root, "")
-
-        else:
-            print("Traversal type " + str(traversal_type) + " is not supported.")
-            return False
 
     def preorder_print(self, start, traversal):
         """Root->Left->Right"""
@@ -78,6 +97,51 @@ class BinaryTree(object):
                 queue.enqeue(node.right)
         return traversal
 
+    def reverse_levelorder_print(self, start):
+        if start is None:
+            return 
+
+        queue = Queue()
+        stack = Stack()
+        queue.enqeue(start)
+
+
+        traversal = ""
+        while len(queue) > 0:
+            node = queue.deqeue()
+
+            stack.push(node)
+
+            if node.right:
+                queue.enqeue(node.right)
+            if node.left:
+                queue.enqeue(node.left)
+        
+        while len(stack) > 0:
+            node = stack.pop()
+            traversal += str(node.value) + "-"
+
+        return traversal
+
+
+
+    
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        elif traversal_type == "inorder":
+            return self.inorder_print(tree.root, "")
+        elif traversal_type == "postorder":
+            return self.postorder_print(tree.root, "")
+        elif traversal_type == "levelorder":
+            return self.levelOrderTraversal(tree.root)
+        elif traversal_type == "reverse levelorder":
+            return self.reverse_levelorder_print(tree.root)
+
+        else:
+            print("Traversal type " + str(traversal_type) + " is not supported.")
+            return False
+
 # 1-2-4-5-3-6-7-
 # 4-2-5-1-6-3-7
 # 4-2-5-6-3-7-1
@@ -98,4 +162,6 @@ tree.root.right.right = Node(7)
 
 #print(tree.print_tree("preorder"))
 #print(tree.print_tree("inorder"))
-print(tree.print_tree("postorder"))
+# print(tree.print_tree("postorder"))
+print(tree.print_tree("levelorder"))
+print(tree.print_tree("reverse levelorder"))
