4b21b7a832ac5ef10fc706437cb1cef83e384f35
mdhvkothari <mdhvkothari007@gmail.com>
2020-09-28 20:02:19 +0530

    commit

--- leetCode/test.py
+++ leetCode/test.py
@@ -1,5 +1,17 @@
-a = "abca"
-for i in range(0,len(a)):
-    s = a[0:i]+a[i+1:len(a)]
-    if s == s[::-1]:
-        print("yes")
+n = 7
+term =4
+arr=[]
+i=2
+j=2
+while(len(arr)!=n):
+        falg=0
+        for j in range(2,i):
+            if i%j == 0:
+                falg =1
+            j+=1
+        if falg != 1:
+            arr.append(i)
+        i+=1
+for i in range(0,len(arr)-1):
+    print(term+arr[i])
+    term = term + arr[i]
