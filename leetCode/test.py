3ca6461337c28443194e0eac585ca538f700d549
mdhvkothari <mdhvkothari007@gmail.com>
2020-09-29 20:11:17 +0530

    commit

--- leetCode/test.py
+++ leetCode/test.py
@@ -1,17 +1,13 @@
-n = 7
-term =4
-arr=[]
-i=2
-j=2
-while(len(arr)!=n):
-        falg=0
-        for j in range(2,i):
-            if i%j == 0:
-                falg =1
-            j+=1
-        if falg != 1:
-            arr.append(i)
-        i+=1
-for i in range(0,len(arr)-1):
-    print(term+arr[i])
-    term = term + arr[i]
+n = int(input())
+arr = list(map(int,input().strip().split()))
+d = {}
+for i in arr:
+    if i not in d:
+        d[i] = 1
+    else:
+        d[i]+=1
+for i in d:
+    if d[i]%2!=0:
+        print(i)
+        break
+
