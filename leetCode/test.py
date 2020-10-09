cfc60c9be2d9c7e8df20bbec71bdff4e0231eb3d
mdhvkothari <mdhvkothari007@gmail.com>
2020-10-09 20:39:07 +0530

    commit

--- leetCode/test.py
+++ leetCode/test.py
@@ -1,48 +1,25 @@
-class Node:
-    def __init__(self,data):
-        self.data = data
-        self.next = None
+l=842
+r= 888
+result= 0
+for i in range(l,r+1):
+    num = i
+    count=0
+    while(num>0):
+        rem = num%2
+        num = num//2
+        if rem == 1:
+            count+=1 
+    if count == 2:
+        result +=1
+    if count>2:
+        flag=0
+        for i in range(2,count):
+            if count%i ==0:
+                flag = 1
+                break
+        if flag==0:
+            result+=1
+        
 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-
-    def adding(self,data):
-        newNode = Node(data)
-        if self.head is None:
-            self.head = newNode
-            return
-        curNode = self.head
-        while curNode.next:
-            curNode = curNode.next
-        curNode.next = newNode
-
-    def print(self):
-        cur_node = self.head
-        while cur_node:
-            print(cur_node.data)
-            cur_node = cur_node.next
-    def rotate(self,number):
-        for i in range(0,number):
-            head_node = self.head
-            cur_node = self.head
-            prev =None
-            while cur_node.next:
-                prev = cur_node
-                cur_node = cur_node.next
-            
-            cur_node.next = self.head
-            self.head = cur_node
-            prev.next = None
-
-
-
-ll = LinkedList()
-ll.adding(10)
-ll.adding(20)
-ll.adding(30)
-ll.adding(40)
-ll.rotate(2)
-ll.print()
+print(result)
