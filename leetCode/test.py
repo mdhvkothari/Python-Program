d4bf37976cce76300d0d4ec3c2b41e7bb0b57080
mdhvkothari <mdhvkothari007@gmail.com>
2020-10-21 20:31:04 +0530

    commit

--- leetCode/test.py
+++ leetCode/test.py
@@ -1,20 +1,23 @@
-name = "xnhtq"
-typed = "xhhttqq"
-dict1={}
-dict2={}
-for i in range(0,len(name)):
-    if name[i] in dict1:
-        dict1[name[i]] +=1
+string = "The quick brown fox jumped over the lazy dog"
+arr = string.split()
+for i in range(0,len(arr)):
+    a = "a"*(i+1)
+    if arr[i][0] in "AEIOUaeiou":
+        arr[i] = arr[i]+"ma"+a
     else:
-        dict1[name[i]] = 1
+        left = arr[i][1:]
+        arr[i] = left+arr[i][0]+"ma"+a
+        
+print(arr)
 
-for i in range(0,len(typed)):
-    if typed[i] in dict2:
-        dict2[typed[i]] +=1
-    else:
-        dict2[typed[i]] = 1
-print(dict1)
-print(dict2)
-for i in dict2:
-    if dict2[i] < dict1[i]:
-        print(False)
+
+
+
+
+
+
+
+
+
+
+
