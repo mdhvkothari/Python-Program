4ce8bc77940787e990f802eb9b5c0749cb211a63
mdhvkothari <mdhvkothari007@gmail.com>
2020-11-10 20:57:33 +0530

    commit

---
+++ leetCode/Reverse Only Letters.py
@@ -0,0 +1,14 @@
+
+S = "a-bC-dEf-ghIj"
+letter = []
+ans = []
+for  i  in range(0,len(S)):
+    if S[i].isalnum():
+        letter.append(S[i])
+for i in S:
+    if i.isalpha():
+        ans.append(letter.pop())
+    else:
+        ans.append(i)
+
+print ("".join(ans))
