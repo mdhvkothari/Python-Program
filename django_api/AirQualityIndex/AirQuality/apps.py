d1feea0bdd46cff7ffc44d20aac534645ad85607
Madhav Kothari <35840824+mdhvkothari@users.noreply.github.com>
2023-09-20 21:08:36 +0530

    Delete django_api directory

--- django_api/AirQualityIndex/AirQuality/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AirqualityConfig(AppConfig):
-    name = 'AirQuality'
--- django_api/AirQualityIndex/AirQuality/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path(r'',views.home,name="home"),
-    path(r'about/',views.about,name="about"),
-    path(r'<url>/',views.article,name="article"),
-
-]
